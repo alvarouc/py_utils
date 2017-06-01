f3bf2c6f995e7e0312e4d1441dadb70fb7a33c94
Alvaro Ulloa <alvarouc@gmail.com>
2017-06-01 12:43:26 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='py_utils',
+      version='0.1.0',
+      description='Python general utilities and code snippets',
+      author='Alvaro Ulloa',
+      author_email='aeulloacerna@geisinger.edu',
+      url='https://github.com/alvarouc/py_utils',
+      packages=['py_utils'],
+     )
