e455f1f2d80f4c4342312852fdc8622cafa74c12
Alvaro Ulloa <alvarouc@gmail.com>
2017-06-01 12:38:01 -0400

    added logger

---
+++ logger/log.py
@@ -0,0 +1,50 @@
+import logging
+
+
+class ProgressConsoleHandler(logging.StreamHandler):
+    """
+    A handler class which allows the cursor to stay on
+    one line for selected messages
+    """
+    on_same_line = False
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            stream = self.stream
+            same_line = hasattr(record, 'same_line')
+            if self.on_same_line and not same_line:
+                stream.write(self.terminator)
+            stream.write(msg)
+            if same_line:
+                stream.write('... ')
+                self.on_same_line = True
+            else:
+                stream.write(self.terminator)
+                self.on_same_line = False
+            self.flush()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
+
+
+def make_logger(name='', path='/tmp/'):
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler(path + name + '.log')
+    fh.setLevel(logging.INFO)
+    fh.setFormatter(formatter)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    # add the handlers to the logger
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    # progress = ProgressConsoleHandler()
+    # logger.addHandler(progress)
+
+    return logger
