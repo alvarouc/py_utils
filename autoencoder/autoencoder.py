7c3b073522bb28f5d312592505750c8dc6fbe23d
Alvaro Ulloa <alvarouc@gmail.com>
2017-07-14 17:20:59 -0400

    started with vae implementation

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -8,6 +8,61 @@ from keras.callbacks import TensorBoard
 log = make_logger('autoencoder')
 
 
+def build_vae(input_dim, ngpu=1, layers_dim=[100, 10, 10],
+              activations=['tanh', 'tanh'],
+              inits=['glorot_uniform', 'glorot_normal'],
+              optimizer='adam', batch_size=512,
+              drop=0.1, l2=1e-5,
+              loss='mse'):
+
+    x = Input(batch_shape=(batch_size, input_dim))
+    h = Dense(intermediate_dim, activation='relu')(x)
+
+    z_mean = Dense(latent_dim)(h)
+    z_log_var = Dense(latent_dim)(h)
+
+    def sampling(args):
+        z_mean, z_log_var = args
+        epsilon = K.random_normal(shape=(batch_size, latent_dim), mean=0.,
+                                  stddev=epsilon_std)
+        return z_mean + K.exp(z_log_var / 2) * epsilon
+
+        # note that "output_shape" isn't necessary with the TensorFlow backend
+        z = Lambda(sampling)([z_mean, z_log_var])
+    # we instantiate these layers separately so as to reuse them later
+    decoder_h = Dense(intermediate_dim, activation='relu')
+    decoder_mean = Dense(original_dim, activation='sigmoid')
+    h_decoded = decoder_h(z)
+    x_decoded_mean = decoder_mean(h_decoded)
+
+    # Custom loss layer
+    class CustomVariationalLayer(Layer):
+        def __init__(self, **kwargs):
+            self.is_placeholder = True
+            super(CustomVariationalLayer, self).__init__(**kwargs)
+
+        def vae_loss(self, x, x_decoded_mean):
+            xent_loss = original_dim * \
+                metrics.binary_crossentropy(x, x_decoded_mean)
+            kl_loss = - 0.5 * K.sum(1 + z_log_var -
+                                    K.square(z_mean) - K.exp(z_log_var),
+                                    axis=-1)
+            return K.mean(xent_loss + kl_loss)
+
+        def call(self, inputs):
+            x = inputs[0]
+            x_decoded_mean = inputs[1]
+            loss = self.vae_loss(x, x_decoded_mean)
+            self.add_loss(loss, inputs=inputs)
+            # We won't actually use the output.
+            return x
+
+    y = CustomVariationalLayer()([x, x_decoded_mean])
+    vae = Model(x, y)
+    vae.compile(optimizer='rmsprop', loss=None)
+    return vae
+
+
 def build_autoencoder(input_dim, ngpu=1, layers_dim=[100, 10, 10],
                       activations=['tanh', 'tanh'],
                       inits=['glorot_uniform', 'glorot_normal'],
@@ -95,3 +150,6 @@ def run_ae(X, epochs=100, batch_size=128, verbose=0,  **kwargs):
     error = ((X2 - Xs)**2).mean(axis=0)
     log.info('Done. Loss %s', ae.evaluate(Xs, Xs))
     return Xp, error, encoder
+
+
+def run_vae(X, epochs=100, batch_size=128, verbose=0,  **kwargs)
