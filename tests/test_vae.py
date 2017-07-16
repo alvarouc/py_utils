d7548a2157104e672b1d759a9e7fa5f6b3bf6310
Alvaro Ulloa <alvarouc@gmail.com>
2017-07-16 00:23:17 -0400

    added test vae

---
+++ tests/test_vae.py
@@ -0,0 +1,33 @@
+import pytest
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import make_blobs
+from sklearn.metrics import silhouette_score
+import sys
+sys.path.append("../")
+
+from autoencoder import run_vae
+print(sys.path)
+X, y = make_blobs(n_samples=1000, n_features=30, centers=3,
+                  random_state=1988)
+
+
+@pytest.mark.medium
+def test_vae():
+    X2, _, _ = run_vae(X, verbose=1, layers_dim=[20, 2])
+    si = silhouette_score(X2, y)
+    plt.figure()
+    plt.scatter(X2[:, 0], X2[:, 1], c=y)
+    plt.savefig('test_vae.png')
+    assert si > 0.8, 'Score {} < 0.8'.format(si)
+    print('Silhouette score: {}'.format(si))
+
+
+def test_vae_2():
+    X2, _, _ = run_vae(X, verbose=1, layers_dim=[20, 20,  2])
+    si = silhouette_score(X2, y)
+    plt.figure()
+    plt.scatter(X2[:, 0], X2[:, 1], c=y)
+    plt.savefig('test_vae2.png')
+    assert si > 0.8, 'Score {} < 0.8'.format(si)
+    print('Silhouette score: {}'.format(si))
