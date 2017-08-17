906b42e27dd987eff06968e2bf67caf33faa661f
Alvaro Ulloa <alvarouc@gmail.com>
2017-08-17 16:54:31 -0400

    epochs to 1000

--- autoencoder/sk_transformer.py
+++ autoencoder/sk_transformer.py
@@ -8,11 +8,13 @@ import pdb
 
 class BaseEncoder(BaseEstimator, TransformerMixin):
     def __init__(self, epochs=1000, batch_size=100, encoding_dim=2,
+                 optimizer='adamax',
                  *args, **kwargs):
         super(BaseEncoder).__init__(*args, **kwargs)
         self.batch_size = batch_size
         self.encoding_dim = encoding_dim
         self.epochs = epochs
+        self.optimizer = optimizer
         self.sc = MinMaxScaler()
 
     def fit(self, X, epochs=100):
@@ -46,7 +48,8 @@ class AutoEncoder(BaseEncoder):
         super(AutoEncoder, self).fit(X, **kwargs)
         self.ae, self.encoder, self.decoder =\
             build_autoencoder(self.Xs.shape[1],
-                              layers_dim=self.layers_dim)
+                              layers_dim=self.layers_dim,
+                              optimizer=self.optimizer)
         self.ae.fit(self.Xs, self.Xs,
                     batch_size=self.batch_size,
                     epochs=self.epochs,
@@ -55,10 +58,9 @@ class AutoEncoder(BaseEncoder):
 
 
 class VariationalAutoEncoder(BaseEncoder):
-    def __init__(self, encoding_dim=2, layer_dim=100,
+    def __init__(self, layer_dim=100,
                  *args, **kwargs):
         super(VariationalAutoEncoder, self).__init__(*args, **kwargs)
-        self.encoding_dim = encoding_dim
         self.layer_dim = layer_dim
 
     def fit(self, X, **kwargs):
@@ -66,7 +68,8 @@ class VariationalAutoEncoder(BaseEncoder):
 
         self.vae, self.encoder, self.decoder =\
             build_vae(self.Xs.shape[1], encoding_dim=self.encoding_dim,
-                      latent_dim=self.layer_dim, batch_size=self.batch_size)
+                      latent_dim=self.layer_dim, batch_size=self.batch_size,
+                      optimizer=self.optimizer)
         self.vae.fit(self.Xs, self.Xs, batch_size=self.batch_size,
                      epochs=self.epochs,
                      shuffle=True, verbose=False)
